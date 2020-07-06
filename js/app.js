c06e7e54143b9749c3624d4b561bb8ee063d0884
etownsend79 <earltownsend1979@gmail.com>
2020-07-06 10:43:57 -0500

    coffee shop first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const navBtn = document.getElementById("navBtn");
+const nav = document.getElementById("nav");
+
+
+navBtn.addEventListener("click", () => {
+    nav.classList.toggle("on");
+})
+
+
+
